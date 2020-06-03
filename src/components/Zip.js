378615c5c0835a5f2a9d2f05eadef35f2c391583
Yusuke Kunishige <ytankengine@gmail.com>
2020-06-03 07:18:56 +0000

    Removed the validate checking (temporarily)

--- src/components/Zip.js
+++ src/components/Zip.js
@@ -5,15 +5,17 @@ function Zip(props) {
     const [validationError, setValidationError] = useState(null);
 
     const validate = (event) => {
-        const zipCodePattern = /^\d{5}$/;
-        const valid = zipCodePattern.test(event.target.value);
-        if (!valid) {
-            setValidationError('* should be a 5 digit number only');
-            props.clearResponse();
-        } else {
-            setValidationError('');
-            props.onZipChange(event.target.value);
-        }
+//        const zipCodePattern = /^\d{5}$/;
+//        const valid = zipCodePattern.test(event.target.value);
+//        if (!valid) {
+//            setValidationError('* should be a 5 digit number only');
+//            props.clearResponse();
+//        } else {
+//            setValidationError('');
+//            props.onZipChange(event.target.value);
+//        }
+		  setValidationError('');
+          props.onZipChange(event.target.value);
     };
 
     return (
