08579863dcdaa872b6af9ae2c6323bca702b66aa
Yusuke Kunishige <ytankengine@gmail.com>
2020-06-04 14:05:50 +0000

    Improved map implementation attempt

---
+++ src/components/GoogleMap.js
@@ -0,0 +1,25 @@
+function GoogleMap() {
+	return (
+	<div id="map"></div>
+    <script>
+      function initMap() {
+        var myLatlng = {lat: -40.9006, lng: 174.8860};
+
+        var map = new google.maps.Map(
+            document.getElementById('map'), {zoom: 7, center: myLatlng});
+          
+        map.addListener('click', function(mapsMouseEvent) {
+
+          var infoWindow = new google.maps.InfoWindow({position: mapsMouseEvent.latLng});
+          infoWindow.setContent(mapsMouseEvent.latLng.toString());
+          infoWindow.open(map);
+        });
+      }
+    </script>
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC-TQvCUYWNNIKtZH7d1nvYntfAuPXWwo0&callback=initMap">
+    </script>
+	)
+}
+
+export default GoogleMap
--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import fetch from 'isomorphic-unfetch'
 import CityResponse from '../components/CityResponse';
 import City from '../components/City';
+import GoogleMap from '../components/GoogleMap';
 
 function AppContainer(props) {
 
@@ -25,6 +26,9 @@ function AppContainer(props) {
                 <div className="col-sm-4"></div>
                 <City onCityChange={handleCityChange} clearResponse={clearResponse}/>
                 <div className="col-sm-4"></div>
+            </div>
+            <div>
+            	<GoogleMap />
             </div>
             <div className="row mt-4">
                 <div className="col-sm-2"></div>
