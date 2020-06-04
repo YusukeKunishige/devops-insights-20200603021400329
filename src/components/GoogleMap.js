e9b4a01158ea4d3849f30cbed95dd5563b08212c
Yusuke Kunishige <ytankengine@gmail.com>
2020-06-04 14:34:04 +0000

    Improved map implementation attempt 3

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -1,9 +1,6 @@
 function GoogleMap() {
-	return (
-	<div>
-	<div id="map"></div>
-    <script>
-      function initMap() {
+	
+	function initMap() {
         var myLatlng = {lat: -40.9006, lng: 174.8860};
 
         var map = new google.maps.Map(
@@ -16,10 +13,10 @@ function GoogleMap() {
           infoWindow.open(map);
         });
       }
-    </script>
-    <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC-TQvCUYWNNIKtZH7d1nvYntfAuPXWwo0&callback=initMap">
-    </script>
+	
+	return (
+	<div>
+	<div id="map"></div>
     </div>
 	)
 }
